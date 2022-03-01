5ba6ea881f8a0b65f8973526c3262192dc49f8b6
lvhaosir6 <985694388@qq.com>
2022-03-01 23:42:47 +0800

    day 120（补充）

--- src/main/java/cc/rcbb/leetcode/topic_34/Solution.java
+++ src/main/java/cc/rcbb/leetcode/topic_34/Solution.java
@@ -1,23 +1,25 @@
 package cc.rcbb.leetcode.topic_34;
 
+import cc.rcbb.leetcode.common.RcbbPrinter;
 /**
  * 34. 在排序数组中查找元素的第一个和最后一个位置
  * https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/
  */
-public class Solution {
+class Solution {
 
     public int[] searchRange(int[] nums, int target) {
-        if (nums.length == 0) {
+        if (nums.length < 1) {
             return new int[]{-1, -1};
         }
         return new int[]{leftSearch(nums, target), rightSearch(nums, target)};
     }
 
-    public int leftSearch(int[] nums, int target) {
+    private int leftSearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
+            //System.out.println("left=" + left + " right=" + right + " mid=" + mid);
             if (nums[mid] == target) {
                 right = mid - 1;
             } else if (nums[mid] > target) {
@@ -25,25 +27,28 @@ public class Solution {
             } else if (nums[mid] < target) {
                 left = mid + 1;
             }
+            //System.out.println("left=" + left + " right=" + right);
         }
-        if (left >= nums.length || nums[left] != target) {
+        if (left > nums.length - 1 || nums[left] != target) {
             return -1;
         }
         return left;
     }
 
-    public int rightSearch(int[] nums, int target) {
+    private int rightSearch(int[] nums, int target) {
         int left = 0;
         int right = nums.length - 1;
         while (left <= right) {
             int mid = left + (right - left) / 2;
+            //System.out.println("left=" + left + " right=" + right + " mid=" + mid);
             if (nums[mid] == target) {
+                left = mid + 1;
+            } else if (nums[mid] < target) {
                 left = mid + 1;
             } else if (nums[mid] > target) {
                 right = mid - 1;
-            } else if (nums[mid] < target) {
-                left = mid + 1;
             }
+            //System.out.println("left=" + left + " right=" + right);
         }
         if (right < 0 || nums[right] != target) {
             return -1;
@@ -52,25 +57,8 @@ public class Solution {
     }
 
     public static void main(String[] args) {
-
         Solution solution = new Solution();
-        //输入: nums = [-1,0,3,5,9,12], target = 9 输出: 4
-        //int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
-        //int target = 9;
-
-
-        //输入: nums = [-1,0,3,5,9,12], target = 2 输出: -1 解释: 2 不存在 nums 中因此返回 -1
-        //int[] nums = new int[]{-1, 0, 3, 5, 9, 12};
-        //int target = 2;
-
-        //int[] nums = new int[]{2, 5};
-        //int target = 2;
-
-
-        int[] nums = new int[]{0, 1, 2, 3, 3};
-        int target = 3;
-
-        int s = solution.rightSearch(nums, target);
-        System.out.println(s);
+        RcbbPrinter.print(solution.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 8));
+        RcbbPrinter.print(solution.searchRange(new int[]{5, 7, 7, 8, 8, 10}, 6));
     }
 }
