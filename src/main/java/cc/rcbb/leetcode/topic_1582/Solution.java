9957fc79a84094d27d3aa749ed39b8bfc3036130
lvhaosir6 <985694388@qq.com>
2022-09-04 21:47:32 +0800

    day 291

---
+++ src/main/java/cc/rcbb/leetcode/topic_1582/Solution.java
@@ -0,0 +1,28 @@
+package cc.rcbb.leetcode.topic_1582;
+
+/**
+ * 1582. 二进制矩阵中的特殊位置
+ * https://leetcode.cn/problems/special-positions-in-a-binary-matrix/
+ */
+class Solution {
+    public int numSpecial(int[][] mat) {
+        int m = mat.length, n = mat[0].length;
+        int[] rowsSum = new int[m];
+        int[] colsSum = new int[n];
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rowsSum[i] += mat[i][j];
+                colsSum[j] += mat[i][j];
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (mat[i][j] == 1 && rowsSum[i] == 1 && colsSum[j] == 1) {
+                    res++;
+                }
+            }
+        }
+        return res;
+    }
+}
