88f2fd1d02d6abac17ab12205b8030d2e8699a5c
lvhaosir <lvhao@umeox.com>
2022-09-07 10:32:30 +0800

    day 293

---
+++ src/main/java/cc/rcbb/leetcode/topic_1592/Solution.java
@@ -0,0 +1,38 @@
+package cc.rcbb.leetcode.topic_1592;
+
+/**
+ * 1592. 重新排列单词间的空格
+ * https://leetcode.cn/problems/rearrange-spaces-between-words/
+ */
+class Solution {
+    public String reorderSpaces(String text) {
+        int length = text.length();
+        String[] words = text.trim().split("\\s+");
+        int cntSpace = length;
+        for (String word : words) {
+            cntSpace -= word.length();
+        }
+        StringBuilder sb = new StringBuilder();
+        if (words.length == 1) {
+            sb.append(words[0]);
+            for (int i = 0; i < cntSpace; i++) {
+                sb.append(' ');
+            }
+            return sb.toString();
+        }
+        int perSpace = cntSpace / (words.length - 1);
+        int restSpace = cntSpace % (words.length - 1);
+        for (int i = 0; i < words.length; i++) {
+            if (i > 0) {
+                for (int j = 0; j < perSpace; j++) {
+                    sb.append(' ');
+                }
+            }
+            sb.append(words[i]);
+        }
+        for (int i = 0; i < restSpace; i++) {
+            sb.append(' ');
+        }
+        return sb.toString();
+    }
+}
