e22f0900f2d084b84c60f3eb87ddc9e6377e4426
lvhaosir <lvhao@umeox.com>
2022-09-13 18:04:31 +0800

    day 298

---
+++ src/main/java/cc/rcbb/leetcode/topic_670/Solution.java
@@ -0,0 +1,32 @@
+package cc.rcbb.leetcode.topic_670;
+
+/**
+ * 670. 最大交换
+ * https://leetcode.cn/problems/maximum-swap/
+ */
+class Solution {
+    public int maximumSwap(int num) {
+        char[] charArray = String.valueOf(num).toCharArray();
+        int n = charArray.length;
+        int maxNum = num;
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                swap(charArray, i, j);
+                maxNum = Math.max(maxNum, Integer.parseInt(new String(charArray)));
+                swap(charArray, i, j);
+            }
+        }
+        return maxNum;
+    }
+
+    public void swap(char[] charArray, int i, int j) {
+        char temp = charArray[i];
+        charArray[i] = charArray[j];
+        charArray[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.maximumSwap(98368));
+    }
+}
