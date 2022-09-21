9c4525af6f4b36199148493fd41fd98dfb0af880
lvhaosir <lvhao@umeox.com>
2022-09-21 10:36:15 +0800

    day 235【854. 相似度为 K 的字符串】

---
+++ src/main/java/cc/rcbb/leetcode/topic_854/Solution.java
@@ -0,0 +1,15 @@
+package cc.rcbb.leetcode.topic_854;
+
+/**
+ * 854. 相似度为 K 的字符串
+ * https://leetcode.cn/problems/k-similar-strings/
+ * <p>
+ * s1要多少次转换为s2
+ */
+class Solution {
+
+    public int kSimilarity(String s1, String s2) {
+
+        return 0;
+    }
+}
