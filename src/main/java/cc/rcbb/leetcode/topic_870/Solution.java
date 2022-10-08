37febd07dd72c268ab11e9d215bd68a1593e6fcf
lvhaosir <lvhao@umeox.com>
2022-10-08 11:25:01 +0800

    day 251【870. 优势洗牌】【贪心】

--- src/main/java/cc/rcbb/leetcode/topic_870/Solution.java
+++ src/main/java/cc/rcbb/leetcode/topic_870/Solution.java
@@ -10,28 +10,28 @@ import java.util.PriorityQueue;
 class Solution {
     public int[] advantageCount(int[] nums1, int[] nums2) {
         int n = nums1.length;
-        int[] res = new int[n];
-        PriorityQueue<int[]> pq = new PriorityQueue<>((int[] p1, int[] p2) -> {
-            return p2[1] - p1[1];
-        });
+        Integer[] idx1 = new Integer[n];
+        Integer[] idx2 = new Integer[n];
         for (int i = 0; i < n; i++) {
-            pq.offer(new int[]{i, nums2[i]});
+            idx1[i] = i;
+            idx2[i] = i;
         }
-        Arrays.sort(nums1);
-        int left = 0;
-        int right = n - 1;
-        while (!pq.isEmpty()) {
-            int[] pair = pq.poll();
-            int i = pair[0];
-            int maxVal = pair[1];
-            if (maxVal < nums1[right]) {
-                res[i] = nums1[right];
-                right--;
-            } else {
-                res[i] = nums1[left];
+        Arrays.sort(idx1, (i, j) -> nums1[i] - nums1[j]);
+        Arrays.sort(idx2, (i, j) -> nums2[i] - nums2[j]);
+
+        int[] res = new int[n];
+        int left = 0, right = n - 1;
+        for (int i = 0; i < n; i++) {
+            if (nums1[idx1[i]] > nums2[idx2[left]]) {
+                res[idx2[left]] = nums1[idx1[i]];
                 left++;
+            } else {
+                // 田忌赛马，使用最小的对上最大的
+                res[idx2[right]] = nums1[idx1[i]];
+                right--;
             }
         }
+
         return res;
     }
 
@@ -39,10 +39,9 @@ class Solution {
         Solution solution = new Solution();
         //输入：A = [2,7,11,15], B = [1,10,4,11]
         //输出：[2,11,7,15]
-        System.out.println(Arrays.toString(solution.advantageCount(new int[]{2, 7, 11, 15}, new int[]{1, 10, 4, 11})));
+        // System.out.println(Arrays.toString(solution.advantageCount(new int[]{2, 7, 11, 15}, new int[]{1, 10, 4, 11})));
         //输入：A = [12,24,8,32], B = [13,25,32,11]
         //输出：[24,32,8,12]
         System.out.println(Arrays.toString(solution.advantageCount(new int[]{12, 24, 8, 32}, new int[]{13, 25, 32, 11})));
-
     }
 }
