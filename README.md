3e59bd75943323ebc46c798c983140d804dca773
lvhaosir <lvhao@umeox.com>
2022-11-14 10:10:13 +0800

    day 285【805. 数组的均值分割】【动态规划、折半搜索】

--- README.md
+++ README.md
@@ -240,7 +240,8 @@
 > [740. 删除并获得点数](https://leetcode-cn.com/problems/delete-and-earn/)
 > [764. 最大加号标志](https://leetcode.cn/problems/largest-plus-sign/)【动态规划】
 > [790. 多米诺和托米诺平铺](https://leetcode.cn/problems/domino-and-tromino-tiling/)【动态规划】  
-> [801. 使序列递增的最小交换次数](https://leetcode.cn/problems/minimum-swaps-to-make-sequences-increasing/)  
+> [801. 使序列递增的最小交换次数](https://leetcode.cn/problems/minimum-swaps-to-make-sequences-increasing/)
+> [805. 数组的均值分割](https://leetcode.cn/problems/split-array-with-same-average/)【动态规划、折半搜索】  
 > [871. 最低加油次数](https://leetcode.cn/problems/minimum-number-of-refueling-stops/)  
 > [873. 最长的斐波那契子序列的长度](https://leetcode.cn/problems/length-of-longest-fibonacci-subsequence/)
 > [913. 猫和老鼠](https://leetcode-cn.com/problems/cat-and-mouse/)  
---
+++ src/main/java/cc/rcbb/leetcode/topic_805/Solution.java
@@ -0,0 +1,94 @@
+package cc.rcbb.leetcode.topic_805;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * 805. 数组的均值分割
+ * https://leetcode.cn/problems/split-array-with-same-average/
+ * <p>
+ * 动态规划、折半搜索
+ */
+class Solution {
+    public boolean splitArraySameAverage(int[] nums) {
+        if (nums.length == 1) {
+            return false;
+        }
+        int n = nums.length, m = n / 2;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        for (int i = 0; i < n; i++) {
+            nums[i] = nums[i] * n - sum;
+        }
+
+        Set<Integer> left = new HashSet<Integer>();
+        for (int i = 1; i < (1 << m); i++) {
+            int tot = 0;
+            for (int j = 0; j < m; j++) {
+                if ((i & (1 << j)) != 0) {
+                    tot += nums[j];
+                }
+            }
+            if (tot == 0) {
+                return true;
+            }
+            left.add(tot);
+        }
+        int rsum = 0;
+        for (int i = m; i < n; i++) {
+            rsum += nums[i];
+        }
+        for (int i = 1; i < (1 << (n - m)); i++) {
+            int tot = 0;
+            for (int j = m; j < n; j++) {
+                if ((i & (1 << (j - m))) != 0) {
+                    tot += nums[j];
+                }
+            }
+            if (tot == 0 || (rsum != tot && left.contains(-tot))) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean splitArraySameAverage1(int[] nums) {
+        if (nums.length == 1) {
+            return false;
+        }
+        int n = nums.length, m = n / 2;
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+        boolean isPossible = false;
+        for (int i = 1; i <= m; i++) {
+            if (sum * i % n == 0) {
+                isPossible = true;
+                break;
+            }
+        }
+        if (!isPossible) {
+            return false;
+        }
+        Set<Integer>[] dp = new Set[m + 1];
+        for (int i = 0; i <= m; i++) {
+            dp[i] = new HashSet<Integer>();
+        }
+        dp[0].add(0);
+        for (int num : nums) {
+            for (int i = m; i >= 1; i--) {
+                for (int x : dp[i - 1]) {
+                    int curr = x + num;
+                    if (curr * n == sum * i) {
+                        return true;
+                    }
+                    dp[i].add(curr);
+                }
+            }
+        }
+        return false;
+    }
+}
